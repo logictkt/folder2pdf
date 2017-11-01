3fd36cccd710211724f9d2304892008e3342536a
logictkt <logictkt@gmail.com>
2017-11-01 16:35:54 +0900

    folder in folder に対応

--- main.rb
+++ main.rb
@@ -1,18 +1,39 @@
 require 'bundler'
 Bundler.require
+require 'fileutils'
 
-puts "Start Folder2PDF !"
-ARGV.each_with_index do |path, i|
-  Dir.chdir(path)
-  folder_name = File.basename(Dir.pwd)
-  puts "Create PDF ... #{folder_name} ..."
+puts "Start Folder2PDF ..."
+
+def run_rmagic files, folder_name
   r = Magick::ImageList.new()
-  files = Dir.glob("*.{jpg,jpeg,JPG,png,PNG}")
   files = files.sort_by{ |s| s.scan(/(\d+)|([^\d]+)/) }
   files.each do |file|
     r.push(Magick::Image.read(file)[0])
   end
   r.write("../#{folder_name}.pdf")
-  puts "Complete create PDF #{folder_name}."
 end
-puts "Finish Folder2PDF !"
+
+def make_pdf path
+  Dir.chdir(path)
+  folder_name = File.basename(Dir.pwd)
+  puts "Create PDF --> #{folder_name}"
+  files = Dir.glob("*.{jpg,jpeg,JPG,png,PNG}")
+  run_rmagic(files, folder_name) unless files.count.zero?
+  # Dir.chdir('../')
+  # FileUtils.rm_rf(folder_name)
+  puts "Complete!"
+end
+
+ARGV.each_with_index do |path, i|
+  Dir.chdir(path)
+  if Dir.exist?("./#{Dir.glob("*").first}")
+    folders = Dir.glob("*")
+    folders.each do |folder|
+      make_pdf(File.join(path, folder))
+    end
+  else
+    make_pdf(path)
+  end
+end
+
+puts "Finish Folder2PDF"
