c57d7dc01aad886b28ccbf820c9bfc5f06c79b7a
logictkt <logictkt@gmail.com>
2017-11-26 17:29:34 +0900

    change sort logic

--- main.rb
+++ main.rb
@@ -4,10 +4,30 @@ require 'fileutils'
 
 puts "Start Folder2PDF ..."
 
-def run_rmagic files, folder_name
+def sort_file_name(file_names)
+  first_word_value_hash = {}
+  sort_target_datas = file_names.map do |file_name|
+                   { sort_title: file_name.scan(/(\d+)|([^\d]+)/).map{ |a| a[1] || a[0].to_i }, base_title: file_name }
+                 end
+  sort_target_datas.each do |name_hash|
+    first_word_value_hash["#{name_hash[:sort_title].first}"] = [] if first_word_value_hash["#{name_hash[:sort_title].first}"].nil?
+    first_word_value_hash["#{name_hash[:sort_title].first}"] << name_hash
+  end
+  first_word_value_hash = first_word_value_hash.sort_by { |key, value| key }
+  result = []
+  first_word_value_hash.each do |first_word, file_name_datas|
+    arr = file_name_datas.sort_by do |file_name_data|
+            file_name_data[:sort_title]
+          end
+    result.concat(arr.map { |file_name_data| file_name_data[:base_title] })
+  end
+  result
+end
+
+def run_rmagic file_names, folder_name
   r = Magick::ImageList.new()
-  files = files,sort_by{ |s| s.scan(/(\d+)|([^\d]+)/).map{ |a| a[1] || a[0].to_i } }
-  files.each do |file|
+  file_names = sort_file_name(file_names)
+  file_names.each do |file|
     r.push(Magick::Image.read(file)[0])
   end
   r.write("../#{folder_name}.pdf")
@@ -17,23 +37,27 @@ def make_pdf path
   Dir.chdir(path)
   folder_name = File.basename(Dir.pwd)
   puts "Create PDF --> #{folder_name}"
-  files = Dir.glob("*.{jpg,jpeg,JPG,png,PNG}")
-  run_rmagic(files, folder_name) unless files.count.zero?
+  file_names = Dir.glob("*.{jpg,jpeg,JPG,png,PNG}")
+  run_rmagic(file_names, folder_name) unless file_names.count.zero?
   # Dir.chdir('../')
   # FileUtils.rm_rf(folder_name)
   puts "Complete!"
 end
 
-ARGV.each_with_index do |path, i|
+def search_dir_make_pdf path
   Dir.chdir(path)
   if Dir.exist?("./#{Dir.glob("*").first}")
     folders = Dir.glob("*")
     folders.each do |folder|
-      make_pdf(File.join(path, folder))
+      search_dir_make_pdf(File.join(path, folder))
     end
   else
     make_pdf(path)
   end
+end
+
+ARGV.each_with_index do |path, i|
+  search_dir_make_pdf(path)
 end
 
 puts "Finish Folder2PDF"
