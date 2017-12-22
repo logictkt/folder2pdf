a764679129e2d20c7d5c3a8270f5ebfa06bae80b
logictkt <logictkt@gmail.com>
2017-12-22 19:51:17 +0900

    ナチュラルソートに変更

--- main.rb
+++ main.rb
@@ -4,29 +4,9 @@ require 'fileutils'
 
 puts "Start Folder2PDF ..."
 
-def sort_file_name(file_names)
-  first_word_value_hash = {}
-  sort_target_datas = file_names.map do |file_name|
-                   { sort_title: file_name.scan(/(\d+)|([^\d]+)/).map{ |a| a[1] || a[0].to_i }, base_title: file_name }
-                 end
-  sort_target_datas.each do |name_hash|
-    first_word_value_hash["#{name_hash[:sort_title].first}"] = [] if first_word_value_hash["#{name_hash[:sort_title].first}"].nil?
-    first_word_value_hash["#{name_hash[:sort_title].first}"] << name_hash
-  end
-  first_word_value_hash = first_word_value_hash.sort_by { |key, value| key }
-  result = []
-  first_word_value_hash.each do |first_word, file_name_datas|
-    arr = file_name_datas.sort_by do |file_name_data|
-            file_name_data[:sort_title]
-          end
-    result.concat(arr.map { |file_name_data| file_name_data[:base_title] })
-  end
-  result
-end
-
 def run_rmagic file_names, folder_name
   r = Magick::ImageList.new()
-  file_names = sort_file_name(file_names)
+  file_names = Naturally.sort(file_names)
   file_names.each do |file|
     r.push(Magick::Image.read(file)[0])
   end
