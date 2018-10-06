431564b4689732488290f6762db6a96ca028d049
logictkt <logictkt@gmail.com>
2018-10-06 22:17:41 +0900

    調整

--- main.rb
+++ main.rb
@@ -2,36 +2,26 @@ require 'bundler'
 Bundler.require
 require 'fileutils'
 
-puts "Start Folder2PDF ..."
+puts 'Start Folder2PDF ...'
 
-def run_rmagic file_names, folder_name
+def run_rmagic(dir, file_names, folder_name)
   file_names = Naturally.sort(file_names)
   image_list = Magick::ImageList.new(*file_names)
-  image_list.write("../#{folder_name}.pdf")
+  image_list.write("#{File.expand_path('..', dir)}/#{folder_name}.pdf")
   image_list.destroy!
 end
 
-def make_pdf path
-  Dir.chdir(path)
-  folder_name = File.basename(Dir.pwd)
-  puts "Create PDF --> #{folder_name}"
-  file_names = Dir.glob("*.{jpg,jpeg,JPG,png,PNG}")
-  run_rmagic(file_names, folder_name) unless file_names.count.zero?
-  Dir.chdir('../')
+def make_pdf(dir)
+  folder_name = File.basename(dir)
+  puts "Create PDF --> #{dir}"
+  file_names = Dir.glob("#{dir}/*.{jpg,jpeg,JPG,png,PNG}")
+  run_rmagic(dir, file_names, folder_name) unless file_names.count.zero?
   # FileUtils.rm_rf(folder_name)
 end
 
-def search_dir_make_pdf path
-  return unless Dir.exist?(path)
-  Dir.chdir(path)
-  if Dir.glob('*').any? { |dir| Dir.exist?("./#{dir}") }
-    Dir.glob('*').each do |folder|
-      next unless Dir.exist?("./#{folder}")
-      search_dir_make_pdf(File.join(path, folder))
-    end
-  else
-    make_pdf(path)
-  end
+def search_dir_make_pdf(path)
+  dirs = Naturally.sort(Dir.glob("#{path}/**/"))
+  dirs.each { |dir| make_pdf(dir) }
 end
 
 ARGV.each do |path|
