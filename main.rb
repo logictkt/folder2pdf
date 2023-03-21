c9b84d83768adf22b0186822bae662cd286e7c0b
logictkt <logictkt@gmail.com>
2023-03-21 13:13:02 +0900

    update

--- main.rb
+++ main.rb
@@ -7,26 +7,29 @@ puts 'Start Folder2PDF ...'
 def run_rmagic(dir, file_names, folder_name)
   file_names = Naturally.sort(file_names)
   image_list = Magick::ImageList.new(*file_names)
-  image_list.write("#{File.expand_path('..', dir)}/#{folder_name}.pdf")
+  image_list.write("#{File.expand_path(dir)}/#{folder_name}.pdf")
   image_list.destroy!
 end
 
-def make_pdf(dir)
+def make_pdf(dir, path)
+  Dir::chdir(dir)
   folder_name = File.basename(dir)
-  file_names = Dir.glob("#{dir}/*.{jpg,jpeg,png,webp}")
+  file_names  = Dir.glob("*.{jpg,jpeg,png,webp}")
   unless file_names.count.zero?
     puts "Create PDF --> #{dir}"
-    run_rmagic(dir, file_names, folder_name)
+    run_rmagic(File::dirname(Dir::pwd), file_names, folder_name)
   end
+  Dir::chdir(path)
   # FileUtils.rm_rf(folder_name)
 end
 
 def search_dir_make_pdf(path)
-  dirs = Naturally.sort(Dir.glob("#{path}/**/"))
+  Dir::chdir(path)
+  dirs = Naturally.sort(Dir.glob("**/"))
   if dirs.empty?
-    make_pdf(path)
+    make_pdf(path, path)
   else
-    dirs.each { |dir| make_pdf(dir) }
+    dirs.each { |dir| make_pdf(dir, path) }
   end
 end
 
